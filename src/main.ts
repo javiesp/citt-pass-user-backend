dc6e3c44a32d8b3034cadc4da22bc12580493987
javiesp <espinajaviera1312@gmail.com>
2024-06-22 12:54:56 -0400

    Deployment backend

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, { // micro servicio
-    transport: Transport.TCP,    
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
     options: {
-      port: 3006 // modificar puerto seg´pun los puerto ocupados 
-    }
+      port: parseInt(process.env.PORT, 10) || 3006, 
+    },
   });
   await app.listen();
 }
 bootstrap();
- 
