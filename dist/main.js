87669be1c12e51a71341c4a9833cf4dd62bba225
javiesp <espinajaviera1312@gmail.com>
2024-06-22 18:25:36 -0400

    Deployment backend fix

--- dist/main.js
+++ dist/main.js
@@ -3,14 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const microservices_1 = require("@nestjs/microservices");
 const app_module_1 = require("./app.module");
+const process = require("process");
 async function bootstrap() {
     const app = await core_1.NestFactory.createMicroservice(app_module_1.AppModule, {
         transport: microservices_1.Transport.TCP,
         options: {
-            port: 3006
-        }
+            port: parseInt(process.env.USERS_SERVICES_PORT, 10) || 3006,
+        },
     });
     await app.listen();
+    console.log(`Microservice 'users' is listening on port ${process.env.USERS_SERVICES_PORT || 3006}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
